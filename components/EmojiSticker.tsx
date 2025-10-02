8125188b667a93717f01a07c67ada712322a6062
noogler-eng <sharadpoddar10102003@gmail.com>
2025-10-02 16:24:27 +0530

    gestures

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -19,9 +19,18 @@ export default function EmojiSticker({ imageSize, stickerSource }: Props) {
   // scaleImage using the useSharedValue() hook. It will take the
   // value of imageSize as its initial value.
   const scaleImage = useSharedValue(imageSize);
+
+  // It is used to create shared values, which are special mutable
+  // values that can be safely accessed and updated on both the
+  // JavaScript thread and the UI thread.
+  // A shared value is like a useState or useRef variable, but
+  // optimized for animations.
   const translateX = useSharedValue(0);
   const translateY = useSharedValue(0);
 
+  // this will allow us to drag the image around the screen
+  // by updating the translateX and translateY shared values
+  // based on the gesture's change in position
   const drag = Gesture.Pan().onChange((event) => {
     translateX.value += event.changeX;
     translateY.value += event.changeY;
@@ -60,6 +69,9 @@ export default function EmojiSticker({ imageSize, stickerSource }: Props) {
     };
   });
 
+  // detecting two gestures (drag and double tap)
+  // we need to wrap the image in two GestureDetectors
+  // one for each gesture
   return (
     <GestureDetector gesture={drag}>
       <Animated.View style={[containerStyle, { top: -350 }]}>
@@ -67,7 +79,7 @@ export default function EmojiSticker({ imageSize, stickerSource }: Props) {
           <Animated.Image
             source={stickerSource}
             resizeMode="contain"
-            style={{ width: imageSize, height: imageSize }}
+            style={[{ width: imageSize, height: imageSize }, imageStyle]}
           />
         </GestureDetector>
       </Animated.View>
